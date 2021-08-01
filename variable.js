6c41d3a7d07e3e87a561ceadde337fbf8f717434
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-01 07:40:33 +0600

    add Fundamental concepts

---
+++ variable.js
@@ -0,0 +1,12 @@
+// Number 
+var watchPrice = 120;
+watchPrice  = 125;
+
+// String 
+var userName = "Nogor Udding";
+
+// Boolean 
+var isExpensive = true;
+
+// Integer : 1 2 4 5 132
+// Float : 34.55 23.555 2.23324 45.2 
