5784d04edeae435452cdaf72f8ffdb0afd135a36
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-01 09:04:36 +0600

    add push and pop

---
+++ push-pop.js
@@ -0,0 +1,18 @@
+var friendsName = ['Sumon', 'Faruk', 'Imran', 'Abu Sayd'];
+console.log(friendsName);
+friendsName.push("Abu Daud");
+friendsName.push("Rasec");
+console.log(friendsName);
+
+var intNumber = [1, 3, 6, 7, 5, 12, 8, 9, 4];
+console.log(intNumber);
+var popitem = intNumber.pop();
+intNumber.pop();
+intNumber.pop();
+intNumber.pop();
+intNumber.pop();
+intNumber.pop();
+intNumber.pop();
+
+console.log(intNumber);
+console.log(popitem);
