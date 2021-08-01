549dc6f9c2bbfae057adb08cb5ca261343962088
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-01 10:13:40 +0600

    add remove items

---
+++ remove.js
@@ -0,0 +1,11 @@
+var arr = [1, 3, 4, 5, 6, 8, 12, 43];
+var shifted = arr.shift();
+var shifted = arr.shift();
+var shifted = arr.shift();
+var shifted = arr.shift();
+console.log(arr);
+console.log(shifted);
+
+var spliced = arr.splice(3, 3);
+console.log(arr);
+console.log(spliced);
