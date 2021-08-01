ca427845dc1409eb54fa70ef7f49f378947afcca
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-01 08:37:58 +0600

    add array index

---
+++ array-index.js
@@ -0,0 +1,12 @@
+var books = ['English', 'Bangla', 'Math', 'Phycis', 'Boilogy'];
+
+var number = [1, 3 , 12, 54, 56, 66, 56, 90];
+
+var banglaBookIndex = books.indexOf('Bangla');
+console.log(banglaBookIndex);
+
+var index = number.indexOf(12);
+console.log(index);
+
+var bookName = books[4];
+console.log(bookName);
