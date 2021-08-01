88bbe566519b2193c7ea68f3c9a5c7775c5fed74
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-01 08:05:41 +0600

    add js array

---
+++ array.js
@@ -0,0 +1,12 @@
+var friendsage = [12, 34, 23, 45];
+
+var oddNumber = [1, 3, 5, 7, 9];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+var playersName = ['Tamim', 'Sakib', 'Taskin'];
+
+console.log(friendsage);
+console.log(oddNumber);
+console.log(playersName);
+console.log(playersName.length);
