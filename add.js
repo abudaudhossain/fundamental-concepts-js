8b1836ba9c94c62bd397c6cb4dc14f3cac116226
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-03 16:55:03 +0600

    add array element

---
+++ add.js
@@ -0,0 +1,14 @@
+var arr = ['bangla', 'englis', 'math'];
+
+var a =  arr.push('boilogy');
+console.log(a);
+console.log(arr);
+
+var whatIs = arr.unshift('physics');
+console.log(whatIs);
+console.log(arr);
+
+var arr2 = ['abu Daud', 'Sumon'];
+
+var newarr = arr.concat(arr2);
+console.log(newarr);
